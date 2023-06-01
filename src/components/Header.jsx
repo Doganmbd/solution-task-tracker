07c228d04dd20e16d19ecddf9fe486144b01648c
mbd <doganmbdogan@gmail.com>
2023-06-01 15:02:56 +0300

    1-Bootstrap,react-icons yüklendi,componentle oluşt

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <h1>TASK TRACKER</h1>
+        <button className='btn btn-danger'>CLOSE ADD TASK BAR</button>
+    </div>
+  )
+}
+
+export default Header
