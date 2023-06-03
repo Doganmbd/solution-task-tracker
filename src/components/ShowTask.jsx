f1bac129c35f046af7df62e89f1b8bbe667395aa
mbd <doganmbdogan@gmail.com>
2023-06-03 09:58:25 +0300

    6-data verisi showtask e aktarıldı ve çarpı butonu

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,10 +1,23 @@
-const ShowTask = () => {
+import { FaTimesCircle } from 'react-icons/fa';
+
+const ShowTask = ({data}) => {
   return (
     <div>
-      <div className="showButon mt-5">
-        <h3>Task</h3>
-        <h6>day</h6>
+      {data.map((data)=> {
+      const {id,day,task,isDone} = data
+
+      return (
+        <div className="showButon mt-5" key={id}>
+        <h3>{task} <FaTimesCircle/></h3>
+        
+        <h6>{day}</h6>
       </div>
+        
+        )
+      
+      
+      })}
+      
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import Header from '../components/Header'
-import ShowTask from '../components/ShowTask'
+import React from "react";
+import Header from "../components/Header";
+import ShowTask from "../components/ShowTask";
+import data from "../helper/starterData";
 
 const Home = () => {
-  return (
-    <div className='container mt-5'>
-        <Header/>
-        <ShowTask/>
-    </div>
-  )
-}
 
-export default Home
+  console.log(data);
+  return (
+    <div className="container mt-5">
+      <Header />
+      <ShowTask data={data}/>
+    </div>
+  );
+};
+
+export default Home;
