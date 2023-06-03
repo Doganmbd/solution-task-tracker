afd3a3901b464157bbf080b37011462833fd82c1
mbd <doganmbdogan@gmail.com>
2023-06-03 14:42:06 +0300

    13-buton name ,rengi şarta bağlandı

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import AddTaskForm from "./AddTaskForm";
 
-const Header = ({localData,setLocalData}) => {
+const Header = ({ localData, setLocalData }) => {
   const [showAdd, setShowAdd] = useState(false);
+  const [change, setChange] = useState({
+    name: "SHOW ADD TASK BAR",
+    bgColor: "purple",
+  });
 
   const handleClick = () => {
+    if (showAdd) {
+      setChange({
+        name: 'SHOW ADD TASK BAR',
+        bgColor: 'purple',
+      });
+    } else {
+      setChange({
+        name: 'CLOSE ADD TASK BAR',
+        bgColor: 'red',
+      });
+    }
     setShowAdd(!showAdd);
   };
 
   return (
     <div className="header">
       <h1>TASK TRACKER</h1>
-      <button onClick={handleClick} className="btn btn-danger btn-lg m-5 shadow p-3 mb-5 ">
-        CLOSE ADD TASK BAR
+      <button
+      style={{ backgroundColor: change.bgColor }}
+        onClick={handleClick}
+        className="btn btn-danger btn-lg m-5 shadow p-3 mb-5 "
+      >
+        {change.name}
       </button>
 
-      {showAdd && <AddTaskForm localData={localData} setLocalData={setLocalData} /> }
-
-      
+      {showAdd && (
+        <AddTaskForm localData={localData} setLocalData={setLocalData} />
+      )}
     </div>
   );
 };
