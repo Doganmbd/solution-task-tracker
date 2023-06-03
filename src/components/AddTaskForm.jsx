65fdcff1827651261853cc6a92d4ae48dd1d17a8
mbd <doganmbdogan@gmail.com>
2023-06-03 14:07:40 +0300

    10-input değri alndı NwTsk e atldı Local e gndldi

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-const AddTaskForm = () => {
+const AddTaskForm = ({ localData, setLocalData }) => {
+  const [task, setTask] = useState("");
+  const [day, setDay] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = new Date().getTime();
+    const newTask = { id: id, task: task, day: day , isDone:false};
+    setLocalData([...localData, newTask]);
+    setTask("");
+    setDay("");
+  };
+
   return (
-    <div >
-      <form className="fs-3">
+    <div>
+      <form onSubmit={handleSubmit} className="fs-3">
         <div className="mb-3 ">
           <label htmlFor="text" className="form-label">
             Task
@@ -13,13 +25,23 @@ const AddTaskForm = () => {
             className="form-control"
             id="text"
             aria-describedby="emailHelp"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            required
           />
         </div>
         <div className="mb-3">
           <label htmlFor="day" className="form-label">
             Day & Time
           </label>
-          <input type="date" className="form-control" id="day" />
+          <input
+            type="date"
+            className="form-control"
+            id="day"
+            value={day}
+            onChange={(e) => setDay(e.target.value)}
+            required
+          />
         </div>
         <div className="d-grid gap-2">
           <button type="submit" className="btn btn-info btn-lg mt-5 ">
