0e4920c27c84e2fe620ec7eeaafb0cbbc4126cbe
mbd <doganmbdogan@gmail.com>
2023-06-03 14:43:35 +0300

    14-Usestate çalışma bilgisi eklendi

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,23 +11,30 @@ const Header = ({ localData, setLocalData }) => {
   const handleClick = () => {
     if (showAdd) {
       setChange({
-        name: 'SHOW ADD TASK BAR',
-        bgColor: 'purple',
+        name: "SHOW ADD TASK BAR",
+        bgColor: "purple",
       });
     } else {
       setChange({
-        name: 'CLOSE ADD TASK BAR',
-        bgColor: 'red',
+        name: "CLOSE ADD TASK BAR",
+        bgColor: "red",
       });
     }
     setShowAdd(!showAdd);
   };
 
+  //! React, default olarak state'leri hemen degistirmeyebilir.
+  //! Ekstra render'lari azaltmak icin state'leri toplu (batch) bir sekilde gunceller.
+  //! Bir event handler icerisindeki ardasik state'ler event bitiminde toplu bir sekilde guncellenmis olur.State'lerin guncelenmesi gelis sirasina gore yapilir.
+  //! Ayni event icerisinde birbirine bagli state'leri kullanirken buna dikkat etmek gerkir.
+
+  //? https://stackoverflow.com/questions/48563650/does-react-keep-the-order-for-state-updates
+
   return (
     <div className="header">
       <h1>TASK TRACKER</h1>
       <button
-      style={{ backgroundColor: change.bgColor }}
+        style={{ backgroundColor: change.bgColor }}
         onClick={handleClick}
         className="btn btn-danger btn-lg m-5 shadow p-3 mb-5 "
       >
