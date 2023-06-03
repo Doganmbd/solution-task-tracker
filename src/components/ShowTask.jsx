d4004abd963d39fa3681dd6fe14f11d1abfceac8
mbd <doganmbdogan@gmail.com>
2023-06-03 13:11:13 +0300

    8- DoubleClick ile butonua css verildi

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -5,9 +5,18 @@ const ShowTask = ({ data }) => {
   const [veri, setVeri] = useState(data);
 
   const handleClickButton = (id) => {
-    console.log(id);
+    /* console.log(id); */
     setVeri(veri.filter((d) => d.id !== id));
   };
+
+  const handleToggle = (id) => {
+    setVeri(
+      veri.map((item) => {
+        return item.id === id ? { ...item, isDone: !item.isDone } : item;
+      })
+    );
+  };
+  /* console.log(veri[0].isDone); */
 
   return (
     <div>
@@ -15,9 +24,15 @@ const ShowTask = ({ data }) => {
         const { id, day, task, isDone } = data;
 
         return (
-          <div className="showButon mt-5" key={id}>
+          <div
+            onDoubleClick={() => {
+              handleToggle(id);
+            }}
+            className={`showButon ${isDone ? "done" : ""} mt-3 `}
+            key={id}
+          >
             <h3>
-              {task}{" "}
+              {task}
               <FaTimesCircle
                 onClick={() => handleClickButton(id)}
                 className="text-danger"
