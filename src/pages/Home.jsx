75167812580b75876939f37a37d3027ad7f5add3
mbd <doganmbdogan@gmail.com>
2023-06-03 14:02:35 +0300

    9-localStorage data gönderme,çekme,güncelleme yap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import ShowTask from "../components/ShowTask";
-import data from "../helper/starterData";
+import { useEffect } from "react";
+/* import data from "../helper/starterData"; */
 
 const Home = () => {
+  const [localData, setLocalData] = useState(
+    JSON.parse(localStorage.getItem("locData")) || []
+  );
 
-  console.log(data);
+  useEffect(() => {
+    localStorage.setItem("locData", JSON.stringify(localData));
+  }, [localData]);
+
+  console.log(localData);
+  /* console.log(data); */
   return (
     <div className="container mt-5">
-      <Header />
-      <ShowTask data={data}/>
+      <Header localData={localData} setLocalData={setLocalData} />
+      <ShowTask
+        localData={localData}
+        setLocalData={setLocalData} /* data={data} */
+      />
     </div>
   );
 };
