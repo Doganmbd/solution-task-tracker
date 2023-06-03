d9a2e84e48eab47e8a59c509774874fb21f09d99
mbd <doganmbdogan@gmail.com>
2023-06-03 12:50:50 +0300

    7- Close butonu yapıldı

--- src/components/ShowTask.jsx
+++ src/components/ShowTask.jsx
@@ -1,23 +1,33 @@
-import { FaTimesCircle } from 'react-icons/fa';
+import { useState } from "react";
+import { FaTimesCircle } from "react-icons/fa";
 
-const ShowTask = ({data}) => {
+const ShowTask = ({ data }) => {
+  const [veri, setVeri] = useState(data);
+
+  const handleClickButton = (id) => {
+    console.log(id);
+    setVeri(veri.filter((d) => d.id !== id));
+  };
+
   return (
     <div>
-      {data.map((data)=> {
-      const {id,day,task,isDone} = data
+      {veri.map((data) => {
+        const { id, day, task, isDone } = data;
 
-      return (
-        <div className="showButon mt-5" key={id}>
-        <h3>{task} <FaTimesCircle/></h3>
-        
-        <h6>{day}</h6>
-      </div>
-        
-        )
-      
-      
+        return (
+          <div className="showButon mt-5" key={id}>
+            <h3>
+              {task}{" "}
+              <FaTimesCircle
+                onClick={() => handleClickButton(id)}
+                className="text-danger"
+              />
+            </h3>
+
+            <h6>{day}</h6>
+          </div>
+        );
       })}
-      
     </div>
   );
 };
